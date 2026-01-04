3c70d22e73f1b6c44821e52cfcfd8384f2c5142b
abhinavbibek <bibekavi22@gmail.com>
2026-01-03 21:10:14 -0800

    after running train_generator.py

---
+++ classifier_models/__init__.py
@@ -0,0 +1,12 @@
+from .vgg import VGG
+from .mobilenetv2 import MobileNetV2
+from .preact_resnet import PreActResNet18
+from .resnet import ResNet18
+from .densenet import DenseNet121
+
+__all__ = [
+    "VGG",
+    "MobileNetV2",
+    "PreActResNet18",
+    "ResNet18",
+]
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 export CUDA_VISIBLE_DEVICES=2
 
-python runAllTrains.py
+
+
+python train_generator.py \
+  --dataset cifar10 \
+  --pc 0.3 \
+  --noise_rate 0.05 \
+  --saving_prefix train_generator_n005_pc03 \
+  --load_checkpoint_clean classifier_clean
+
