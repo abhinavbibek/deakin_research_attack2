067b4e32ac8af8d212d4a2ccfaf7a90a46fa239e
Bibek Bhandari <110053125+abhinavbibek@users.noreply.github.com>
2026-02-08 11:23:10 +0545

    Simplify run_celeba.sh
    Removed commented sections and updated script to directly run training scripts for clean classifier, generator, victim, and evaluation.

--- run_celeba.sh
+++ run_celeba.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=4
-export TMPDIR=/raid/home/dgxuser10/tmp
-export TEMP=/raid/home/dgxuser10/tmp
-export TMP=/raid/home/dgxuser10/tmp
 
-# Standard COMBAT Setting (5% overall poisoning)
-# For CelebA (8 classes), target class is ~12.5% of data.
-# To get 5% overall, we need pc = 5 / 12.5 = 0.4
-# (Using pc=0.4 poisons 40% of the target class)
+python train_clean_classifier.py --dataset celeba --saving_prefix classifier_clean_celeba
 
-# 1. Train Clean Classifier (if not already done)
-#python train_clean_classifier.py --dataset celeba --saving_prefix classifier_clean_celeba
-
-# 2. Train Generator
-# noise_rate 0.0392 + scale_noise_rate 2.0 (implicit in code) = Standard Paper Setting
-python train_generator.py \
+python train_generator_celeba.py \
   --dataset celeba \
   --pc 0.4 \
   --noise_rate 0.0392 \
   --saving_prefix train_generator_celeba_pc04_n00392 \
   --load_checkpoint_clean classifier_clean_celeba
 
-# 3. Train Victim
-python train_victim.py \
+
+python train_victim_celeba.py \
   --dataset celeba \
   --pc 0.4 \
   --noise_rate 0.0392 \
   --saving_prefix train_victim_celeba_pc04_n00392 \
   --load_checkpoint train_generator_celeba_pc04_n00392_clean
 
-# 4. Evaluation
-python eval.py \
+
+python eval_celeba.py \
   --dataset celeba \
   --pc 0.4 \
   --noise_rate 0.0392 \
