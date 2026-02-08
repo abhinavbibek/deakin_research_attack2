fcbe13b23ef46647e5cdbc6cb8ae77ff04136d67
Bibek Bhandari <110053125+abhinavbibek@users.noreply.github.com>
2026-02-08 12:32:55 +0545

    Update run_cifar10.sh

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -2,7 +2,11 @@ export CUDA_VISIBLE_DEVICES=5
 
 python train_clean_classifier.py --dataset cifar10 --saving_prefix classifier_clean
 
-python train_generator.py \
+# Paper Setting: Overall Poisoning Rate (p) = 5% (0.05)
+# Calculation: pc (Target Class Poisoning) = p * NumClasses
+# For CIFAR-10 (10 Classes): pc = 0.05 * 10 = 0.5
+
+python train_generator_cifar10.py \
   --dataset cifar10 \
   --pc 0.5 \
   --noise_rate 0.0392 \
@@ -10,7 +14,7 @@ python train_generator.py \
   --load_checkpoint_clean classifier_clean
 
 
-python train_victim.py \
+python train_victim_cifar10.py \
   --dataset cifar10 \
   --pc 0.5 \
   --noise_rate 0.0392 \
@@ -18,7 +22,7 @@ python train_victim.py \
   --load_checkpoint train_generator_pc05_n00392_tuned_clean
 
 
-python eval.py \
+python eval_cifar10.py \
   --dataset cifar10 \
   --pc 0.5 \
   --noise_rate 0.0392 \
